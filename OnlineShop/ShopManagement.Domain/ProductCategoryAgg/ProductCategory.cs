af12828e1d2907fbc006eea56b7261eaf72a71d3
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2023-06-06 00:30:42 +0330

    initail

---
+++ OnlineShop/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _0_FrameWork.Domain;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public class ProductCategory : EntityBase
+    {
+        public long Id { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
