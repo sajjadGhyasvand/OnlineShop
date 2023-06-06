4886ba4e15f66a2d03b8a97aa6afa16bcbf54413
sajjadGhyasvand <sajadGh1995@hotmail.com>
2023-06-06 16:49:20 +0330

    .

---
+++ OnlineShop/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory GetById(long id);
+        List<ProductCategory> GetAll();
+    }
+}
