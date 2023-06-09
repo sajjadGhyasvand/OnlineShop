59d95092e6a1f448e936d286105b1753d8aa5aa0
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2023-06-09 22:06:13 +0330

    .

---
+++ OnlineShop/ServiceHost/Program.cs
@@ -0,0 +1,30 @@
+using ShopManagement.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionstring = builder.Configuration.GetConnectionString("OnlineShopDB");
+ShopManagementBootstrapper.Configure(builder.Services, connectionstring);
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
