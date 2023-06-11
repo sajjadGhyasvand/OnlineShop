c241a64507d197249c87d3e266cc3e5f74c6472f
sajjadGhyasvand <sajadGh1995@hotmail.com>
2023-06-11 15:24:23 +0330

    rtl page

---
+++ OnlineShop/ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
