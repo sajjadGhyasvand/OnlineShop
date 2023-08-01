1c261c8bb658d08a09e671be8233cee0a2737571
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-08-01 17:00:27 +0330

    UI Index PT.5

---
+++ OnlineShop/ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using _01_Query.Contract.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly _01_Query.Contract.ProductCategory.IProductCategoryQuery _productCategoryQuery;
+
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery=productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
