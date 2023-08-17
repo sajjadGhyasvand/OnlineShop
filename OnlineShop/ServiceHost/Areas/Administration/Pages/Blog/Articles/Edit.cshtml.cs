f6ffa47ee0faf8a371bb61b570a3dfb78a2568dc
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-08-17 16:49:47 +0330

    Compelete Edit ANd Cread Article But (DatePicker and  text Editor Failed)

--- OnlineShop/ServiceHost/Areas/Administration/Pages/Blog/Articles/Edit.cshtml.cs
+++ OnlineShop/ServiceHost/Areas/Administration/Pages/Blog/Articles/Edit.cshtml.cs
@@ -1,12 +1,33 @@
+using BlogManagment.Application.Contracts.Article;
+using BlogManagment.Application.Contracts.ArticleCategory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ServiceHost.Areas.Administration.Pages.Blog.Articles
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        public SelectList ArticleCategories;
+        public EditArticle Command;
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+        private readonly IArticleApplication _articleApplication;
+
+        public EditModel(IArticleCategoryApplication articleCategoryApplication, IArticleApplication articleApplication)
         {
+            _articleCategoryApplication=articleCategoryApplication;
+            _articleApplication=articleApplication;
+        }
+        public void OnGet(long id)
+        {
+            Command = _articleApplication.GetDetails(id);
+            ArticleCategories = new SelectList(_articleCategoryApplication.GetArticleCategories(), "Id", "Name");
+
+        }
+        public IActionResult OnPost(EditArticle command)
+        {
+            var result = _articleApplication.Edit(command);
+            return RedirectToPage("./Index");
         }
     }
 }
