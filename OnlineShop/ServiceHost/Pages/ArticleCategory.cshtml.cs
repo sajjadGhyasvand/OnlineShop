46e6f8aa9bb7707cf5d844b20730aea535c3d932
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-09-05 16:37:11 +0330

    Article Category Page PT.2

--- OnlineShop/ServiceHost/Pages/ArticleCategory.cshtml.cs
+++ OnlineShop/ServiceHost/Pages/ArticleCategory.cshtml.cs
@@ -1,3 +1,5 @@
+using _01_Query.Contract.Article;
+using _01_Query.Contract.ArticleCategory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,25 @@ namespace ServiceHost.Pages
 {
     public class ArticleCategoryModel : PageModel
     {
-        public void OnGet()
+        public ArticleCategoryQueryModel ArticleCategory;
+        public List<ArticleCategoryQueryModel> ArticleCategories;
+        public List<ArticleQueryModel> LatestArticles;
+
+        private readonly IArticleQuery _articleQuery;
+        private readonly IArticleCategoryQuery _articleCategoryQuery;
+
+        public ArticleCategoryModel(IArticleCategoryQuery articleCategoryQuery, IArticleQuery articleQuery)
         {
+            _articleQuery = articleQuery;
+            _articleCategoryQuery = articleCategoryQuery;
+        }
+
+        public void OnGet(string id)
+        {
+            LatestArticles = _articleQuery.LatestArticles();
+            ArticleCategory = _articleCategoryQuery.GetArticleCategory(id);
+            ArticleCategories = _articleCategoryQuery.GetArticleCategories();
         }
     }
 }
+
